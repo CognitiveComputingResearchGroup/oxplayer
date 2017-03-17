6487fea44321689ecc43162e12a17ee0e6f5dc13
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2017-03-17 05:24:33 +0000

    fixes resetting board

--- src/env/environment.py
+++ src/env/environment.py
@@ -2,14 +2,13 @@
 #  coding=utf-8
 from functools import partial
 
-
 import lidapy
 from std_msgs.msg import String
-
 
 # Topic definitions
 board_state_topic = lidapy.Topic('oxplayer/env/board', msg_type=String)
 action_topic = lidapy.Topic('oxplayer/env/action', msg_type=String)
+
 
 def first_blank(board):
     for i in range(len(board)):
@@ -18,55 +17,67 @@ def first_blank(board):
     return i
 
 
-
 class OXPlayerEnvironment(object):
-
-    _board = [-1]*9
-    _agent_turn = False
-    _opponent = partial(first_blank, board=_board)
-
     def __init__(self, **kwargs):
-        self._start = True
+        self._board = [-1] * 9
+        self._agent_turn = False
+        self._opponent = partial(first_blank, board=self._board)
+
         lidapy.init(process_name='Environment')
         self.mainloop()
 
     def board_string(self):
-        board_template = '{}│{}│{}\n' \
+        board_template = '\n' \
+                         '{}│{}│{}\n' \
                          '─┼─┼─\n' \
                          '{}│{}│{}\n' \
                          '─┼─┼─\n' \
                          '{}│{}│{}\n'
-        board_marks = {1:'X', 0:'O', -1:' '}
+        board_marks = {1: 'X', 0: 'O', -1: ' '}
         ox_board = (board_marks[mark] for mark in self._board)
         return board_template.format(*ox_board)
 
-    def _reset_board(self):
-        _board = [-1]*9
-        _agent_turn = False
+    def _is_end(self):
+        return -1 not in self._board
 
-    def receive_move(self):
+    def _reset_board(self):
+        for i in range(len(self._board)):
+            self._board[i] = -1
+        self._agent_turn = False
+
+    @staticmethod
+    def receive_action():
         msg = action_topic.receive(timeout=1)
-        move = -1
+        move = 9
         if msg:
-            lidapy.loginfo('Env recvd:'+msg)
+            lidapy.loginfo('Env recvd:' + msg)
             move = int(msg)
         return move
 
+    def _make_move(self, pos, agent_turn):
+        if -1 < pos < 9:
+            if self._board[pos] == -1:
+                mark = int(not agent_turn)
+                self._board[pos] = mark
+                return True
+        return False
+
     def mainloop(self):
         while True:
-            if not self._agent_turn:
-                self._board[self._opponent()] = 1
-                lidapy.loginfo('Opponent played')
-                self._agent_turn = not self._agent_turn
-            else:
-                move = self.receive_move()
-                if move is not -1:
+            if self._is_end():
+                self._reset_board()
+
+            if self._agent_turn:
+                move = OXPlayerEnvironment.receive_action()  # receive_action should be bound to an instance
+                if move != 9:
                     lidapy.loginfo('Player plays')
-                    self._board[move] = 0
-                    self._agent_turn = not self._agent_turn
+            else:
+                move = self._opponent()
+                lidapy.loginfo('Opponent plays')
+
+            if self._make_move(move, self._agent_turn):
+                self._agent_turn = not self._agent_turn
 
             lidapy.loginfo(self.board_string())
             board_state_topic.send(self._board)
             print('running')
-
-
