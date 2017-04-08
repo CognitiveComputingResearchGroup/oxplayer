9bc215a42b18578d1beab3b87df7e702d8fb5ef3
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2017-04-08 07:04:57 +0000

    fixes run bugs, cleans code complete environment control.

--- src/sensory_memory.py
+++ src/sensory_memory.py
@@ -2,21 +2,43 @@
 from sys import argv
 
 import lidapy
-from lidapy import LIDAThread, Config
-from std_msgs.msg import String
-from env.environment import first_blank
+import ast
+from lidapy import Config
+from lidapy.modules import SensoryMemory
+from env.environment import first_blank, PLAYER2, PLAYER1
+from lidapy import Task
 
 board_state_topic = lidapy.Topic('oxplayer/env/board')
-blank_position = lidapy.Topic('oxplayer/blank', msg_type=String)
+pain_signal_topic = lidapy.Topic('oxplayer/player2/pain')
+blank_position = lidapy.Topic('oxplayer/blank')
+turn_topic = lidapy.Topic('oxplayer/env/turn')
+dorsal_turn_topic = lidapy.Topic('oxplayer/player2/dorsal/turn')
 
 
 def see_board():
-    msg = board_state_topic.receive(timeout=1)
-    if msg:
-        lidapy.loginfo(msg)
-        blank = first_blank(msg)
+    board_state = board_state_topic.receive()
+    lidapy.loginfo('Received board_state:'+str(board_state))
+    if board_state:
+        board_state = ast.literal_eval(board_state)
+        blank = first_blank(board_state)
         lidapy.loginfo('blank position:'+str(blank))
         blank_position.send(blank)
 
-lidapy.init(config=Config(argv[1]), process_name='sensory_memory')
-LIDAThread(name='sensory_memory', callback=see_board).start()
+
+def dorsal_update():
+    turn = turn_topic.receive()
+    lidapy.loginfo('Received turn:'+str(turn))
+    if turn == PLAYER1 or turn == PLAYER2:
+        dorsal_turn_topic.send(turn)
+        lidapy.loginfo('Published to '+str(dorsal_turn_topic)+': '+str(turn))
+
+
+sensory_memory = SensoryMemory()
+agent_config = Config()
+agent_config.set_param('rate_in_hz', 1)
+lidapy.init(config=agent_config)
+
+
+sensory_memory.tasks.append(Task(name='see_board', callback=see_board))
+sensory_memory.tasks.append(Task(name='dorsal_stream', callback=dorsal_update))
+sensory_memory.start()
--- src/sensory_motor_memory.py
+++ src/sensory_motor_memory.py
@@ -1,21 +1,42 @@
 #! /usr/bin/env/ python
-from sys import argv
 
 import lidapy
-from lidapy import LIDAThread, Config
+from lidapy import Config
+from lidapy import Task
+from lidapy.modules import SensoryMotorMemory
+from env.environment import PLAYER1, PLAYER2
 
 
 blank_position = lidapy.Topic('oxplayer/blank')
-action_topic = lidapy.Topic('oxplayer/env/action')
+action_topic = lidapy.Topic('oxplayer/player2/action')
+dorsal_turn_topic = lidapy.Topic('oxplayer/player2/dorsal/turn')
+
+sms = SensoryMotorMemory()
+sms.turn = PLAYER1
 
 
-def see_board():
-    msg = blank_position.receive(timeout=1)
-    if msg:
+def make_move():
+    msg = blank_position.receive()
+    lidapy.loginfo('Received blank_position:'+str(msg))
+    lidapy.loginfo('Current turn:'+str(sms.turn))
+    if msg is not None and sms.turn == PLAYER2:
         lidapy.loginfo(msg)
         lidapy.loginfo('motor move:'+str(msg))
-        action_topic.send(str(int(msg)+1))
+        action_topic.send(msg)
 
 
-lidapy.init(config=Config(argv[1]), process_name='sensory_motor_memory')
-LIDAThread(name='sensory_motor_memory', callback=see_board).start()
+def receive_turn():
+    turn = dorsal_turn_topic.receive()
+    lidapy.loginfo('Received turn:'+str(turn))
+    if turn:
+        sms.turn = int(turn)
+
+
+agent_config = Config()
+agent_config.set_param('rate_in_hz', 1)
+lidapy.init(config=agent_config)
+
+sms.tasks.append(Task(name='make_move', callback=make_move))
+sms.tasks.append(Task(name='receive_turn', callback=receive_turn))
+
+sms.start()
